c7dfdea2c1490fae4e57de4ea05f1046441eb08a
Stefan Dikov <118777573+spderman3333@users.noreply.github.com>
2026-08-06 19:27:28 -0700

    Sim works, turns out the gear ratio was backward.

--- src/main/java/frc/robot/subsystem/Elevator.java
+++ src/main/java/frc/robot/subsystem/Elevator.java
@@ -10,6 +10,8 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.MotorAlignmentValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.sim.ChassisReference;
+import com.ctre.phoenix6.sim.TalonFXSimState;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.networktables.DoublePublisher;
@@ -19,14 +21,19 @@ import edu.wpi.first.units.Units;
 import edu.wpi.first.units.measure.*;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.simulation.ElevatorSim;
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.util.Color8Bit;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Robot;
-import frc.robot.util.FusedElevatorSimMech2d;
 
-import java.util.concurrent.atomic.AtomicReference;
 
 import static edu.wpi.first.units.Units.Meters;
+import static frc.robot.subsystem.ElevatorConstants.*;
 
 /**
  * Class for testing motion profiling on Maelstrom.
@@ -36,6 +43,8 @@ public class Elevator extends SubsystemBase {
 
     private final TalonFX topMotor = new TalonFX(ElevatorConstants.TOP_MOTOR_CAN_ID);
     private final TalonFX bottomMotor = new TalonFX(ElevatorConstants.BOTTOM_MOTOR_CAN_ID);
+
+    private final TalonFXSimState topMotorSimState = topMotor.getSimState();
 
     // Cache the control request so we don't have to keep making a new object everytime.
     private final PositionVoltage motorControlScheme;
@@ -58,7 +67,11 @@ public class Elevator extends SubsystemBase {
     private DoublePublisher bottomMotorRotationPublisher; // In Rotations
 
     private ElevatorSim elevatorSim;
-    private FusedElevatorSimMech2d fusedElevatorSimMech2d;
+
+    private Mechanism2d elevatorMechanism;
+    private MechanismRoot2d elevatorMechanismRoot;
+    private MechanismLigament2d elevatorMechanismLigament;
+
 
     public Elevator(NetworkTableInstance ntInstance) {
         elevatorLoggingNT = ntInstance.getTable("Subsystems/Elevator");
@@ -68,6 +81,8 @@ public class Elevator extends SubsystemBase {
         topMotor.getConfigurator().apply(ElevatorConstants.MOTOR_CONFIG);
         bottomMotor.getConfigurator().apply(ElevatorConstants.MOTOR_CONFIG);
 
+        topMotorSimState.Orientation = ChassisReference.Clockwise_Positive;
+
         // No need to specifically instruct the bottom motor.
         bottomMotor.setControl(new Follower(ElevatorConstants.TOP_MOTOR_CAN_ID, MotorAlignmentValue.Aligned));
 
@@ -76,37 +91,59 @@ public class Elevator extends SubsystemBase {
         motorControlScheme = new PositionVoltage(currentPositionSetpoint.getAngleOfMotor());
 
         if (Robot.isSimulation()) {
-            /*
-             * In the CAD, a 12t gear turns a 70t gear.
-             * The 70t gear is attached to a 22t gear with a pitch diameter of
-             */
-            elevatorSim = new ElevatorSim(
-                DCMotor.getKrakenX60(2),
-                // Motors are attached to a 12t gear which turns a 70t gear (which is attached to a 22t gear), simplfied to 6/35
-                6.0/35.0, // Reminder 6/35 = 0, because of integer division truncation, use doubles.
-                Units.Pounds.of(13.92).in(Units.Kilograms),
-                // Calculated the Pitch diameter of WCP-0560 divide by 2 for radius
-                Units.Inches.of(1.75667).in(Meters)/2.0,
-                0,
-                Units.Inches.of(15.75).in(Meters),
-                true,
-                0);
+            elevatorSim = constructElevatorSim();
 
-            fusedElevatorSimMech2d = new FusedElevatorSimMech2d(
-                elevatorSim,
-                elevatorLoggingNT,
-                6.0/35.0,
-                Units.Inches.of(1.75667),
-                Units.Inches.of(15.75),
-                topMotor, bottomMotor);
+            var subsystemNetworkTableName = getSubsystemNameFromNetworkTable(elevatorLoggingNT);
+
+            // Todo: add a method to resize elevatorMechanism and change its background color.
+            // Note: Width and height are in meters.
+            elevatorMechanism = new Mechanism2d(MAXIMUM_ELEVATOR_HEIGHT.in(Meters)*1.5, MAXIMUM_ELEVATOR_HEIGHT.in(Meters)*1.75, new Color8Bit(Color.kBlue));
+            elevatorMechanismRoot = elevatorMechanism.getRoot(subsystemNetworkTableName + " Root", MAXIMUM_ELEVATOR_HEIGHT.in(Meters)*0.75,MAXIMUM_ELEVATOR_HEIGHT.in(Meters)*0.5);
+            elevatorMechanismLigament = elevatorMechanismRoot.append(new MechanismLigament2d(subsystemNetworkTableName + " Ligament", 0, 90, 5, new Color8Bit(Color.kOrange)));
+            // Todo: do the rest of the set up here.
+
+            // Display the elevator mechanism to SmartDashboard
+            SmartDashboard.putData(subsystemNetworkTableName + " Sim", elevatorMechanism);
         }
     }
 
     @Override
     public void simulationPeriodic() {
-        fusedElevatorSimMech2d.runPeriodic();
+
+        topMotorSimState.setSupplyVoltage(12); // This might be unneeded.
+        double leaderMotorVoltage = topMotor.getMotorVoltage().getValueAsDouble();
+        elevatorSim.setInputVoltage(leaderMotorVoltage);
+
+        elevatorSim.update(0.02);
+        topMotorSimState.setRawRotorPosition(convertElevatorLinearToMotorAngular(elevatorSim.getPositionMeters()));
+        topMotorSimState.setRotorVelocity(convertElevatorLinearToMotorAngular(elevatorSim.getVelocityMetersPerSecond()));
+
+        elevatorMechanismLigament.setLength(elevatorSim.getPositionMeters());
     }
 
+    /**
+     * Converts linear measures from the elevator (i.e. position, velocity) to motor angular values
+     * @param elevatorLinearValueMeters Value of the elevators positon (meters)/velocity (meters/sec)
+     * @return The value converted for the motor in the gearbox.
+     */
+    private double convertElevatorLinearToMotorAngular(double elevatorLinearValueMeters) {
+        return (elevatorLinearValueMeters/(2*Math.PI*ElevatorConstants.PULLY_RADIUS.in(Meters)))*ElevatorConstants.MOTOR_TO_ELEVATOR_GEARING;
+    }
+
+    /**
+     *  Parses the name of the subsystem from a given NetworkTable instance.
+     * @param subsystemNetworkTable A reference to the subsystem NetworkTable.
+     * @return The right most "folder" in the path, in this case, it will be the name of the subsystem.
+     */
+    private static String getSubsystemNameFromNetworkTable(NetworkTable subsystemNetworkTable) {
+        /*
+         The NetworkTable path has the "folders" separated by forward slashes, so we can split it into an array.
+         Then we can grab the last entry in the array.
+         */
+        String[] elevatorNetworkTablePath = subsystemNetworkTable.getPath().split("/");
+        return elevatorNetworkTablePath[elevatorNetworkTablePath.length-1];
+        // Todo: add unit tests for this method.
+    }
 
     @Override
     public void periodic() {
@@ -127,10 +164,8 @@ public class Elevator extends SubsystemBase {
         TrapezoidProfile.State endingState = new TrapezoidProfile.State(elevatorMotorPosition.getAngleOfMotor().in(Units.Rotations), 0);
 
         return startRun(
-            ()->{
-            motionProfilingTimer.restart();
-            },
-            ()->{
+            () -> motionProfilingTimer.restart(),
+            () -> {
                 double timeSinceStartOfControl = motionProfilingTimer.get();
                 // Units of calculatedPosRots will be in rotations.
                 TrapezoidProfile.State calculatedPosRots = motionProfile.calculate(
@@ -144,35 +179,9 @@ public class Elevator extends SubsystemBase {
         ).until(() -> motionProfile.isFinished(motionProfilingTimer.get()));
     }
 
-    /**
-     * Nested class to allow for quick configuration of the constants in the Elevator code.
-     */
-    public static class ElevatorConstants {
-        public static final int TOP_MOTOR_CAN_ID = 16;
-        public static final int BOTTOM_MOTOR_CAN_ID = 17;
-
-
-        public static final TalonFXConfiguration MOTOR_CONFIG = new TalonFXConfiguration()
-            .withMotorOutput(
-                new MotorOutputConfigs()
-                    .withInverted(InvertedValue.Clockwise_Positive)
-                    .withNeutralMode(NeutralModeValue.Brake))
-            .withSlot0(new Slot0Configs()
-                .withKP(10)
-                .withKI(0)
-                .withKD(0)
-                .withKS(0)
-                .withKV(1)
-                .withKA(0)
-                .withKG(0));
-
-
-        // Constants for the Configuration of the Trapezoidal Profile.
-        public static final AngularVelocity MAX_ELEVATOR_VELOCITY = Units.RotationsPerSecond.of(5);
-        public static final AngularAcceleration MAX_ELEVATOR_ACCELERATION = Units.RotationsPerSecondPerSecond.of(2);
-
-        // Same time period as the RoboRIO cycles
-        public static final double MOTION_PROFILING_DELTA_TIME = 0.02;
+    public Command setMotorPositonWithoutMP(ElevatorMotorPosition elevatorMotorPosition) {
+        System.out.println("Set motor Position without MP:" + elevatorMotorPosition.getAngleOfMotor());
+        return runOnce(() -> topMotor.setControl(motorControlScheme.withPosition(elevatorMotorPosition.getAngleOfMotor())));
     }
 
     /**
