c5ac950d6a4ec1cc31a7bceac71ae144c1d0a63a
Stefan Dikov <118777573+spderman3333@users.noreply.github.com>
2026-08-07 15:57:55 -0700

    Added a simple wrapper class around gear ratios to ensure simulation doesn't fail because of a silly issue next time.
    For those just tuning in, I got the gear ratio backward, and that caused a world of hurt.

---
+++ src/main/java/frc/robot/util/GearRatio.java
@@ -0,0 +1,86 @@
+package frc.robot.util;
+
+/**
+ * An immutable wrapper class around a gear ratio, in order to ensure gear ratios are created correctly, and ready for simulation.
+ */
+public class GearRatio {
+
+    /**
+     * Number of teeth on the driving (input) gear.
+     */
+    public final int numberOfDrivingTeeth;
+
+    /**
+     * Number of teeth on the driven (output) gear.
+     */
+    public final int numberOfDrivenTeeth;
+
+    /**
+     * The gear ratio from the ensuing two gears, in the ratio of "driven:driving."
+     * This is also known as the "reduction" between the two gears.
+     * This field will work for WPILib's <a href=https://github.wpilib.org/allwpilib/docs/release/java/edu/wpi/first/wpilibj/simulation/LinearSystemSim.html>LinearSystemSim</a> classes.
+     */
+    public final double gearRatio;
+
+    /**
+     * The type of gear ratio this object is.
+     */
+    public final GearRatioType gearRatioType;
+
+    /**
+     * Constructs a gear ratio from the given input.
+     *
+     * @param numberOfDrivingTeeth Number of teeth on the driving (input) gear.
+     * @param numberOfDrivenTeeth Number of teeth on the driven (output) gear.
+     */
+    public GearRatio(int numberOfDrivingTeeth, int numberOfDrivenTeeth) {
+        this.numberOfDrivingTeeth = numberOfDrivingTeeth;
+        this.numberOfDrivenTeeth = numberOfDrivenTeeth;
+
+        gearRatio = (double) numberOfDrivenTeeth / numberOfDrivingTeeth;
+
+        if (gearRatio > 1.0) {
+            gearRatioType = GearRatioType.UNDERDRIVE;
+        } else if (gearRatio == 1.0) {
+            gearRatioType = GearRatioType.DIRECT_DRIVE;
+        } else if (gearRatio < 1.0) {
+            gearRatioType = GearRatioType.OVERDRIVE;
+        } else {
+            throw new RuntimeException("\"Math ain't mathing,\" gearRatio: \"" + gearRatio + "\" is invalid, and some how not a number!");
+        }
+    }
+
+    public double getGearRatio() {
+        return gearRatio;
+    }
+
+    public int getNumberOfDrivenTeeth() {
+        return numberOfDrivenTeeth;
+    }
+
+    public int getNumberOfDrivingTeeth() {
+        return numberOfDrivingTeeth;
+    }
+
+    public GearRatioType getGearRatioType() {
+        return gearRatioType;
+    }
+
+    /**
+     * Represents the different types of gear ratios (reduction/underdrive, direct drive, and overdrive).
+     */
+    public enum GearRatioType {
+        /**
+         * Gear reduction, input smaller than the output, so the ratio is &gt; 1:1 (e.g. 2:1, 3:1)
+         */
+        UNDERDRIVE,
+        /**
+         * The ratio is 1:1, so there is no difference in torque or speed, except the driven gear spins opposite the driving gear.
+         */
+        DIRECT_DRIVE,
+        /**
+         * Input is larger than the output, so the ratio is &lt; 1:1 (e.g 0.8:1, 0.75:1)
+         */
+        OVERDRIVE
+    }
+}
